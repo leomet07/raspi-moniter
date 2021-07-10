3b3b23ee1a2792db2d4e562c57bbee19aa24cdd6
leomet07 <leomet07@gmail.com>
2021-07-09 20:50:03 -0400

    Configurable data logger minutes

--- data_logger/main.py
+++ data_logger/main.py
@@ -1,6 +1,9 @@
 import os
 import time
+from dotenv import load_dotenv
+load_dotenv()
 from run_speedtest import run_speedtest
 while True:
 	run_speedtest()
-	time.sleep(60 * 30)
+	minutes =  os.getenv("SLEEPMIN") if os.getenv("SLEEPMIN") else 4
+	time.sleep(60 * minutes)
